c95eb29c4495fc7b95773ee711ccb915d48749fa
Kevin Chen <Kevinkeeeh@gmail.com>
2021-10-02 12:12:18 +0200

    V0.3

---
+++ js/code.js
@@ -0,0 +1,3 @@
+$(document).ready(function(){
+  $(".fadein").hide(0).delay(250).fadeIn(1500)
+});
---
+++ js/index-code.js
@@ -0,0 +1,6 @@
+setTimeout(function() {
+  $(".loader-wrapper").fadeOut(500);
+}, 500)
+setTimeout(function() {
+  window.location.assign('kezdolap.html');
+}, 1500)
