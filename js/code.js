602687a1cee65553f10a579704f2663635ec51bd
Kevin Chen <Kevinkeeeh@gmail.com>
2021-10-03 16:41:20 +0200

    V0.4

--- js/code.js
+++ js/code.js
@@ -1,3 +1,35 @@
 $(document).ready(function(){
-  $(".fadein").hide(0).delay(250).fadeIn(1500)
+  $(".fadein, .home, #rolunk, #temakorok, #osszefoglalo").hide(0);
+  $(".fadein, .home").fadeIn(1500);
 });
+
+function logoFunction(){
+  $(".home, #rolunk, #temakorok, #osszefoglalo").delay(150).fadeOut(500);
+  $(".home").delay(500).fadeIn(500)
+}
+
+function NavFunction(event){
+  $(".home, #rolunk, #temakorok, #osszefoglalo").delay(150).fadeOut(500);
+  let page = $(event.target).attr('class').split(" ")
+  console.log("Tartalom cserélése a következőre: "+page[1])
+  $(".home").delay(150).fadeOut(500);
+  $("#"+page[1]).delay(500).fadeIn(500)
+}
+
+function dropdown(event){
+  let flip = $(event.target).attr('class').split(" ")
+  if (flip[0] == "flip1"){
+    $("#panel1, #panel2, #panel3").slideToggle("slow");
+    $("#panel4, #panel5, #panel6, #panel7, #panel8, #panel9").slideUp();
+  } else if (flip[0] == "flip2"){
+    $("#panel4, #panel5, #panel6").slideToggle("slow");
+    $("#panel1, #panel2, #panel3, #panel7, #panel8, #panel9").slideUp();
+  } else if (flip[0] == "flip3"){
+    $("#panel7, #panel8, #panel9").slideToggle("slow");
+    $("#panel4, #panel5, #panel6, #panel1, #panel2, #panel3").slideUp();
+  }
+}
+
+$("button").click(function(){
+  $("div").animate({left: '250px'});
+});  
