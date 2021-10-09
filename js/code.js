dddaa4688faf11aa517dd774a0c92938c586c5c7
Kevin Chen <Kevinkeeeh@gmail.com>
2021-10-09 12:53:12 +0200

    V0.6.2

--- js/code.js
+++ js/code.js
@@ -40,35 +40,38 @@ function NavFunction(event) {
 }
 
 /* LEGÖRDÜLŐ MENÜ ANIMÁCIÓ */
-function dropdown(event) {
+function dropdown(event) {            /* PÉLDÁUL FLIP1 */
   kep = document.querySelector("." + event.target.className.substring(0, 5) + " " + ".arrow");
   flip = $(event.target).attr('class').split(" ")
   if (flip[0] == "flip1") {
-    arrowRotate();
-    $("#panel1, #panel1, #panel1").slideToggle("slow");
+    arrowRotate(event);
+    $("#arrow2, #arrow3").removeClass("arrow-down");
+    $("#arrow2, #arrow3").addClass("arrow-up");
     $("#panel2, #panel3").slideUp();
+    $("#panel1, #panel1, #panel1").slideToggle("slow");
   } else if (flip[0] == "flip2") {
-    arrowRotate();
-    $("#panel2, #panel2, #panel2").slideToggle("slow");
+    arrowRotate(event);
+    $("#arrow1, #arrow3").removeClass("arrow-down");
+    $("#arrow1, #arrow3").addClass("arrow-up");
     $("#panel1, #panel3").slideUp();
+    $("#panel2, #panel2, #panel2").slideToggle("slow");
   } else if (flip[0] == "flip3") {
-    arrowRotate();
+    arrowRotate(event);
+    $("#arrow2, #arrow1").removeClass("arrow-down");
+    $("#arrow2, #arrow1").addClass("arrow-up");
+    $("#panel2, #panel1").slideUp();
     $("#panel3, #panel3, #panel3").slideToggle("slow");
-    $("#panel1, #panel2").slideUp();
   }
 }
 
-/* NYÍL FORDULÁSÁNAK ANIMÁCIÓJA */
-function arrowRotate() {
-  if (arrowup == undefined) { arrowup == false }
-  if (arrowup == false) {
-    kep.classList.add("arrow-up")
-    kep.classList.remove("arrow-down")
-    arrowup = true;
-  } else {
+/* NYÍL FORDULÁSÁNAK ANIMÁCIÓJA */ /* HA RENDELKEZIK AZ ADOTT OSZTALLYAL, MEGKAPJA AZ ELLENKEZOJET */
+function arrowRotate(event) { /* ALAPBÓL FELFELÉ NÉZ */
+  if (kep.classList.contains("arrow-up")){
     kep.classList.add("arrow-down")
     kep.classList.remove("arrow-up")
-    arrowup = false;
+  } else {
+    kep.classList.add("arrow-up")
+    kep.classList.remove("arrow-down")
   }
 }
 
